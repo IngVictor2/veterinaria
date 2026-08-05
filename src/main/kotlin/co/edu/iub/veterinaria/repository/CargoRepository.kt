fedf75688334ae75f076c8a4e068200baf875d79
IngVictor2 <victorjrmartinez06@gmail.com>
2026-08-05 13:37:30 -0500

    chore: eliminar codigo sin uso (findByNombre Cargo, findByNombreUsuario, getClienteId) y archivo basura raiz

--- src/main/kotlin/co/edu/iub/veterinaria/repository/CargoRepository.kt
+++ src/main/kotlin/co/edu/iub/veterinaria/repository/CargoRepository.kt
@@ -4,5 +4,4 @@ import co.edu.iub.veterinaria.model.Cargo
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CargoRepository : JpaRepository<Cargo, Int> {
-    fun findByNombre(nombre: String): Cargo?
 }
--- src/main/kotlin/co/edu/iub/veterinaria/repository/UsuarioRepository.kt
+++ src/main/kotlin/co/edu/iub/veterinaria/repository/UsuarioRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface UsuarioRepository : JpaRepository<Usuario, Int> {
-    fun findByNombreUsuario(nombreUsuario: String): Usuario?
 
     @Query("""
         SELECT u FROM Usuario u
